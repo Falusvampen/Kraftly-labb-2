7c778531b9a3250815499a8ed022506c499d3bc6
AndryanL <ryan94@live.se>
2026-08-27 11:16:16 +0200

    Format

--- src/utils/format.test.js
+++ src/utils/format.test.js
@@ -1,8 +1,14 @@
 import { it, expect } from "vitest";
-import { formatKwh } from "./format";
+import { formatKwh, formatAmount } from "./format";
 
 it("should format kWh values correctly", () => {
-  const value = "1204";
+  const value = "730";
   const result = formatKwh(value);
-  expect(result).toBe("1 204 kr");
+  expect(result).toBe("730 kWh");
 });
+
+it("should formatAmount (kr)", () => {
+    const value= "1204";
+    const result = formatAmount(value);
+    expect(result).toBe("1 204 kr");
+}
